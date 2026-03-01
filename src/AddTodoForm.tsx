0a05182739bb6778a5a753abdcc5eb931826cd72
Jan Muljowin <janmuljowin@gmail.com>
2026-03-01 21:28:32 +0100

    Added Enter to add Todo

--- src/AddTodoForm.tsx
+++ src/AddTodoForm.tsx
@@ -9,7 +9,14 @@ interface AddTodoFromProbs{
 export function AddTodoForm(probs: AddTodoFromProbs){
 
 return <div><div className="flex gap-4 items-center">
-        <input className="px-2 py-1 border border-gray-300 focus:outline-sky-500" type="text" name="todoInput" value={probs.value} onChange={probs.onChange} id="" placeholder="New entry"/>
+        <input className="px-2 py-1 border border-gray-300 focus:outline-sky-500" type="text" name="todoInput" value={probs.value} onChange={probs.onChange} id="" placeholder="New entry" 
+        onKeyDown={
+            (e) => {
+              if (e.key === "Enter"){
+              probs.onAdd()
+              }
+            }
+        }/>
         <button className="px-2 py-1 bg-sky-500 text-white hover:bg-sky-600" onClick={probs.onAdd}>Add</button>
       </div></div>
 
